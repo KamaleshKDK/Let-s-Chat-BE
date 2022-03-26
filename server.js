9458234462e245ee36cb2293909d369aa151fd84
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-26 11:44:53 +0530

    netlify link added

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://lets-chat-kamalesh-k13h11.netlify.app/",
     // credentials: true,
   },
 });
